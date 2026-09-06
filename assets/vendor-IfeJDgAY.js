5d6b41349e91cc888db1f37ece7fbb2ef892258b
dsdevsingh556-png <dsdevsingh556@gmail.com>
2026-09-06 16:25:44 +0530

    fix(pages): restore full vendor-IfeJDgAY.js (62308)

--- assets/vendor-IfeJDgAY.js
+++ assets/vendor-IfeJDgAY.js
@@ -1,9 +1,1 @@
-import{g as Pi}from"./react-BWtM72Fx.js";var Ka={exports:{}},Wa={};/**
- * @license React
- * scheduler.production.min.js
- *
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
+PLACEHOLDER_LOAD_FROM_FILE
