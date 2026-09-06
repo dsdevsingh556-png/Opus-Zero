b2a1b43817205dffeffc88d16c75d1ea4621b8c9
dsdevsingh556-png <dsdevsingh556@gmail.com>
2026-09-06 16:19:33 +0530

    fix(submit-ready): remaining chunks on gh-pages (HO-SR-007)

--- assets/vendor-IfeJDgAY.js
+++ assets/vendor-IfeJDgAY.js
@@ -1,1 +1,9 @@
-FILE:/workspace/push-content/vendor-IfeJDgAY.js
+import{g as Pi}from"./react-BWtM72Fx.js";var Ka={exports:{}},Wa={};/**
+ * @license React
+ * scheduler.production.min.js
+ *
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
